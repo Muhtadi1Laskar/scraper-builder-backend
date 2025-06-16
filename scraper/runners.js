4ec557c4dedf4d7d5b4a623a6cf989d1455415f3
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-06-16 15:34:21 +0600

    Moved scraper functions object to scraper.js file

--- scraper/runners.js
+++ scraper/runners.js
@@ -1,57 +1,6 @@
 import { chromium } from 'playwright';
-import { scrapeStrategies } from './scraperStrategy.js';
+import { scrapeMultiple, scrapeSingle } from './scrapers.js';
 
-const scrapeMultiple = async (page, config) => {
-  const fieldsArray = {};
-
-  for (const selector of config.selectors) {
-    const strategyType = scrapeStrategies[selector.type];
-    if (!strategyType) continue;
-
-    const strategyFn = strategyType[selector.mode];
-    if (!strategyFn) continue;
-
-    try {
-      fieldsArray[selector.name] = await strategyFn(page, selector);
-    } catch (error) {
-      fieldsArray[selector.name] = [];
-      console.error(`Error scraping ${selector.name}`, err);
-    }
-  }
-
-  const keys = Object.keys(fieldsArray);
-  const length = Math.max(...keys.map(k => fieldsArray[k].length));
-  const results = [];
-
-  for (let i = 0; i < length; i++) {
-    const row = {};
-    for (const key of keys) {
-      row[key] = fieldsArray[key][i] || null;
-    }
-    results.push(row);
-  }
-  return results;
-}
-
-const scrapeSingle = async (page, config) => {
-  const results = {};
-
-  for (const selector of config.selectors) {
-    const strategyType = scrapeStrategies[selector.type];
-    if (!strategyType) continue;
-
-    const strategyFn = strategyType[selector.mode];
-    if (!strategyFn) continue;
-
-    try {
-      results[selector.name] = await strategyFn(page, selector);
-    } catch (error) {
-      results[selector.name] = [];
-      console.error(`Error scraping ${selector.name}, `, error);
-    }
-  }
-  return results;
-}
 
 export async function scrapeFromConfig(config) {
   const browser = await chromium.launch({ headless: false });
---
+++ scraper/scrapers.js
@@ -0,0 +1,53 @@
+import { scrapeStrategies } from "./scraperStrategy.js";
+
+export const scrapeMultiple = async (page, config) => {
+    const fieldsArray = {};
+
+    for (const selector of config.selectors) {
+        const strategyType = scrapeStrategies[selector.type];
+        if (!strategyType) continue;
+
+        const strategyFn = strategyType[selector.mode];
+        if (!strategyFn) continue;
+
+        try {
+            fieldsArray[selector.name] = await strategyFn(page, selector);
+        } catch (error) {
+            fieldsArray[selector.name] = [];
+            console.error(`Error scraping ${selector.name}`, err);
+        }
+    }
+
+    const keys = Object.keys(fieldsArray);
+    const length = Math.max(...keys.map(k => fieldsArray[k].length));
+    const results = [];
+
+    for (let i = 0; i < length; i++) {
+        const row = {};
+        for (const key of keys) {
+            row[key] = fieldsArray[key][i] || null;
+        }
+        results.push(row);
+    }
+    return results;
+}
+
+export const scrapeSingle = async (page, config) => {
+    const results = {};
+
+    for (const selector of config.selectors) {
+        const strategyType = scrapeStrategies[selector.type];
+        if (!strategyType) continue;
+
+        const strategyFn = strategyType[selector.mode];
+        if (!strategyFn) continue;
+
+        try {
+            results[selector.name] = await strategyFn(page, selector);
+        } catch (error) {
+            results[selector.name] = [];
+            console.error(`Error scraping ${selector.name}, `, error);
+        }
+    }
+    return results;
+}
