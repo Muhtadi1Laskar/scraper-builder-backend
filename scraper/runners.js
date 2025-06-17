70b92fd5a73093ffe38b34bffc2c767ee7cc23ef
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-06-17 09:14:09 +0600

    Added website name field to response

--- scraper/runners.js
+++ scraper/runners.js
@@ -7,15 +7,19 @@ export async function scrapeFromConfig(config) {
   const page = await browser.newPage();
   await page.goto(config.targetURL);
 
+  const title = await page.title();
+
   const multipleMode = config.selectors.some(sel => sel.mode === 'multiple');
 
   if (multipleMode) {
     const data = await scrapeMultiple(page, config);
+    data.push({ websiteName: title });
     await browser.close();
     return data;
 
   } else {
     const datas = await scrapeSingle(page, config);
+    datas.push({ websiteName: title });
     await browser.close();
     return datas;
   }
